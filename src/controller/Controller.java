a6f3e10c8e9eb044a0e6a0f50cd052a08b39cf94
Nemanja <xwealthx@gmail.com>
2018-03-19 13:18:14 +0100

    Controller message

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,4 +1,100 @@
-package se.kth.sda.controller;
+package controller;
 
-public class Controller {
+import javafx.print.Printer;
+import se.kth.sda.model.GarageManager;
+import se.kth.sda.model.InspectionManager;
+
+
+public class Controller {    
+    
+    private GarageManager garageManager;
+    private InspectionManager inspectionManager;
+    private Printer printer;
+    private CreditCardProcessingUnit creditCardProcessingUnit;
+    
+    /**
+     * Creates controller andd makes new Garage manager, Ispection manager
+     * and Printer objects.
+     */    
+    public Controller() {
+        garageManager = new GarageManager();
+        inspectionManager = new InspectionManager();
+        printer = new Printer();                
+    }
+    
+    /**
+     * 
+     */
+    public void startNewInspection{
+        garageManager.nextCustomer();
+        
+        garageManager.closeDoor();
+    }
+    
+    /**
+     * 
+     * @param regNumber
+     * @return 
+     */
+    public double getInspectionCost(String regNumber){
+        return inspectionManager.getInspectionCost();        
+    }
+    
+    /**
+     * 
+     * @param creditCard
+     * @return 
+     */
+    public boolean authorisePayment(CreditCardDTO creditCard){        
+        CreditCardProcessingUnit creditCardProcessingUnit = new CreditCardProcessingUnit;   //???
+        return creditCardProcessingUnit.authorisePayment();        
+    }    
+        
+    /**
+     * 
+     * @return 
+     */
+    public String getNextInspection(){
+        inspectionManager.getNextInspection()
+    }
+    
+    /**
+     * 
+     * @param result 
+     */
+    public void enterInspectionResult(String result){
+        inspectionManager.enterInspectionnResult();
+    }
+    
+    /**
+     * 
+     * @param receipt 
+     */
+    public void printReceipt(ReceiptDTO receipt){
+        printer.printReceipt();
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public boolean hasNextInspection(){        
+        return inspectionManager.hasNextInspection();
+    }
+    
+    /**
+     * 
+     * @param regNumber 
+     */
+    public void saveInspectionResult(String regNumber){
+        inspectionManager.saveinspectionResult();
+    }
+    
+    /**
+     * 
+     */
+    public void printResult(){
+        printer.printInspectionResult();
+    }
 }
+
