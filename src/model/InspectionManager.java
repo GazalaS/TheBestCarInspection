6347bff8e57a7d09a3faed91b61116a10877d2fa
Aaron Sum <sumsx03@gmail.com>
2018-03-19 11:32:12 +0100

    Add backup for astah and InspectionManager.java

--- src/model/InspectionManager.java
+++ src/model/InspectionManager.java
@@ -1,4 +1,46 @@
-package se.kth.sda.model;
+package model;
 
 public class InspectionManager {
+	private List<InspectionInfoDTO> inspectionCheckList;
+	private VehicleDBHandler vehicleDB;
+	private Printer printer;
+
+	public InspectionManager(Printer printer) {
+		vehicleDB = new VihicleDBHandler();
+		this.printer = new Printer();
+		inspectionCheckList = new ArrayList<>();
+	}
+	
+	public double getInspectionCost(String regNumber) {
+		return 0.00; //TODO
+	}
+	
+	private void getInspectionCheckList(String regNumber) {
+		//TODO
+	}
+	
+	private void CalculateInspectionCost() {
+		
+	}
+	
+	public boolean hasNextInspection() {
+		return true;//TODO
+	}
+	
+	public void getNextInspection() {
+		//TODO
+	}
+	
+	public void enterInspectionResult(String result) {
+		//TODO
+	}
+	
+	public void saveInspectionResult(String regNumber) {
+		//TODO
+	}
+	
+	public void printInspectionResult() {
+		//TODO
+	}
+	
 }
