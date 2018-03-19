645b47a812dbfc802205bbf5f8080506f01cc5a8
Aaron Sum <sumsx03@gmail.com>
2018-03-19 13:05:02 +0100

    Add implementation for the class.

--- src/model/InspectionManager.java
+++ src/model/InspectionManager.java
@@ -1,46 +1,58 @@
 package model;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import integration.InspectionInfoDTO;
+import integration.VehicleDBHandler;
+import integration.Printer;
+
 public class InspectionManager {
 	private List<InspectionInfoDTO> inspectionCheckList;
 	private VehicleDBHandler vehicleDB;
 	private Printer printer;
+	private double inpsectionCost;
+	private int inspectionCheckListIndexTracker;
 
 	public InspectionManager(Printer printer) {
 		vehicleDB = new VihicleDBHandler();
-		this.printer = new Printer();
-		inspectionCheckList = new ArrayList<>();
+		this.printer = printer;
+		inspectionCheckListIndexTracker = 0;
 	}
-	
+
 	public double getInspectionCost(String regNumber) {
-		return 0.00; //TODO
+		inspectionCheckList = vehicleDB.getInspectionCheckList(regNumber);
+		calculateInspectionCost();
 	}
-	
-	private void getInspectionCheckList(String regNumber) {
-		//TODO
+
+	private void calculateInspectionCost() {
+		inspectionCost = inspectionCheckList.stream().map(item -> item.getPrice()).reduce(0, Double::sum);
 	}
-	
-	private void CalculateInspectionCost() {
-		
-	}
-	
+
 	public boolean hasNextInspection() {
-		return true;//TODO
+		if (inspectionCheckListIndexTracker < inspectionCheckList.size()) {
+			return true;
+		}
+		return false;
 	}
-	
-	public void getNextInspection() {
-		//TODO
+
+	public String getNextInspection() {
+		String nextInspectionItem = inspectionCheckList.get(inspectionCheckListIndexTracker);
+		inspectionCheckListIndexTracker++;
+		return nextInspectionItem;
 	}
-	
+
 	public void enterInspectionResult(String result) {
-		//TODO
+		boolean isPass = (result.toLowerCase().equals(result.toLowerCase())) ? true : false;
+		inspectionCheckList.get(inspectionCheckListIndexTracker - 1).setInspectionResult(isPass);
 	}
-	
+
 	public void saveInspectionResult(String regNumber) {
-		//TODO
+		vehicleDB.saveInspectionCheckList(regNumber, inspectionCheckList);
 	}
-	
+
 	public void printInspectionResult() {
-		//TODO
+		List<InspectionInfoDTO> inpsectionResult = inspectionCheckList.stream()
+				.filter(item -> item.getInspectionResult() == false).collect(Collectors.toList());
+		printer.printInspectionResult(inpsectionResult);
 	}
-	
 }
