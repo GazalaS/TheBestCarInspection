e78aa728ce16b7afa8506f5132adab23df31a413
Temporary SDA User 1175 <tmp-sda-1175@kth.se>
2018-03-19 13:54:22 +0100

    Added class files in integration package

--- src/integration/Display.java
+++ src/integration/Display.java
@@ -1,4 +1,24 @@
-package se.kth.sda.integration;
+package integration;
+
+/**
+ * Controls the queuenumber display.
+ * This is the only clas updating the display. 
+ * @author GazalaS <gazalafshaikh@gmail.com>
+ */
 
 public class Display {
+    
+    /**
+     * Connects to the display and shows the number zero.
+     */
+    public Display(){
+        displayNo(0);
+    }    
+    /**
+     * When this method returns, the display is showing the specified number.
+     * @param number The number to show.
+     */
+    public void displayNo(int number){
+        System.out.println("The display is now showing: " + number);
+    }
 }
---
+++ src/integration/InspectionInfoDTO.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package integration;
+
+/**
+ * Contains information of individual checklist entry along with it price and inspection result
+ * 
+ * @author GazalaS <gazalafshaikh@gmail.com>
+ */
+
+public class InspectionInfoDTO {
+    private String inspectionInformation;
+    private double price;
+    private boolean inspectionResult;
+    
+   /**
+    * 
+    * @param inspectionInformation
+    * @param price 
+    */
+    
+    public InspectionInfoDTO(String inspectionInformation, double price, boolean inspectionResult){
+        this.inspectionInformation = inspectionInformation;
+        this.price = price;
+        this.inspectionResult = inspectionResult;
+    }
+    
+    /**
+     * Returns price for an inspection  
+     * @return double
+     */
+    public double getPrice(){
+        return price;
+    }
+    
+   /**
+    * Returns information for an inspection  
+    * @return 
+    */
+    public String getInspectionInformation(){
+        return inspectionInformation;
+    }
+    
+    /**
+     * Returns result for an inspection
+     * @return 
+     */
+    public boolean getInspectionResult(){
+        return inspectionResult;
+    }
+    
+    /**
+     * Set the result of the inspection
+     * @param result true/false 
+     */
+    public void setInspectionResult(boolean result){
+        this.inspectionResult = result;
+    }
+}
