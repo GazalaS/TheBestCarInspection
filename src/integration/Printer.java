89a67443b556a51d90458963a4306a950cd72e90
abdullahjameel <abdula.j.m.89@hotmail.com>
2018-03-20 15:51:56 +0100

    updated the printer class

--- src/integration/Printer.java
+++ src/integration/Printer.java
@@ -6,6 +6,7 @@ import java.util.List;
 /**
  *
  * This class will print the receipt and the inspection result
+ *
  * @author Abdullah.
  *
  */
@@ -15,15 +16,18 @@ public class Printer {
     }
 
     /**
-     * This method will print the car register number, date and the inspection cost.
+     * This method will print the car register number, date and the inspection
+     * cost.
+     *
      * @param receipt hold all the receipt information.
      */
     public void printReceipt(ReceiptDTO receipt) {
-        SimpleDateFormat formatter=new SimpleDateFormat("dd-MMM-yyyy");
-        System.out.println("RegNumber:" + receipt.getRegNumber());
-        System.out.println("ReceiptDate:" + formatter.format(receipt.getReceiptDate()));
-        System.out.println("Cost:" + receipt.getCost());
-      
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
+        System.out.println("Registration Number: " + receipt.getRegNumber());
+        System.out.println("Receipt Date: " + formatter.format(receipt.getReceiptDate()));
+        System.out.println("Cost: " + receipt.getCost());
+
     }
 
     /**
@@ -31,9 +35,13 @@ public class Printer {
      * @param inspectionResult this parameter will hold the inspection results.
      */
     public void printInspectionResult(List<InspectionInfoDTO> inspectionResult) {
-        System.out.println("Inspection results:");
-        for (InspectionInfoDTO ins : inspectionResult) {
-            System.out.println(ins.getInspectionInformation() + ":" + (ins.getInspectionResult() ? "pass" : "fail"));
+        if (!inspectionResult.isEmpty()) {
+            System.out.println("Inspection results:");
+            for (InspectionInfoDTO ins : inspectionResult) {
+                System.out.println(ins.getInspectionInformation() + ":" + (ins.getInspectionResult() ? "pass" : "fail"));
+            }
+        } else {
+            System.out.println("Passed all inspections");
         }
     }
 
